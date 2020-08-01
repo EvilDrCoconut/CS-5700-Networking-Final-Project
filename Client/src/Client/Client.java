ac34a7af1142ce9f0e98df5e28298ca49fa09d6a
skyenygaard <skyenygaard@gmail.com>
2020-08-01 18:42:56 -0400

    added HTML extractor

--- Client/src/Client/Client.java
+++ Client/src/Client/Client.java
@@ -4,67 +4,47 @@ import java.awt.Desktop;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Client {
 
-  static public int webPort = 2000;
-  static public int packetSize = 1500;
+  public static final int WEB_PORT = 2000;
+  public static final int PACKET_SIZE = 1500;
+  // Localhost is 127.0.0.1
+  // TODO ****
+  InetAddress host = InetAddress.getByName("127.0.0.1");
+  private final String baseUrl;
+  private String cacheFolder = System.getProperty("user.home") + File.separator + "web-cache";
+  private String optionalParameters = "";
+  private String writePath;
+  private final String requestedFileString;
 
-  static void addCacheFolder() throws IOException {
-    if (!Files
-        .isDirectory(Paths.get(System.getProperty("user.home") + File.separator + "web-cache"))) {
-      Files.createDirectory(
-          Paths.get(System.getProperty("user.home") + File.separator + "web-cache"));
-    }
-  }
+  Client(String requestedFileString) throws UnknownHostException {
+    this.requestedFileString = requestedFileString;
+    baseUrl = requestedFileString.substring(0, requestedFileString.lastIndexOf("/") + 1);
 
-  static String createDirectories(String savePath, String serverPath) throws IOException {
-    if (!Files.isDirectory(Paths.get(savePath))) {
-      throw new FileNotFoundException("Make sure to pass a valid save path.");
-    } else {
-      String[] folders = serverPath.split("/");
-      String pathSoFar = savePath;
-      System.out.println(pathSoFar);
-      for (int i = 0; i < folders.length - 1; i++) {
-        pathSoFar = pathSoFar + File.separator + folders[i];
-
-        if (!Files
-            .isDirectory(Paths.get(pathSoFar))) {
-          Files.createDirectory(
-              Paths.get(pathSoFar));
-        }
-      }
-      pathSoFar = pathSoFar + File.separator + folders[folders.length - 1];
-      return pathSoFar;
-    }
 
   }
 
-  static String buildDefaultFilePath(String serverPath) throws IOException {
-    addCacheFolder();
-    return createDirectories(
-        System.getProperty("user.home") + File.separator + "web-cache", serverPath);
-  }
-
-
-  static String getURL(String input) {
-    String url;
-    if (input.indexOf('/') != -1) {
-      url = input.substring(0, input.indexOf('/'));
-    } else {
-      url = input;
-    }
-    return url;
-  }
+//
+//  static String getURL(String input) {
+//    String url;
+//    if (input.indexOf('/') != -1) {
+//      url = input.substring(0, input.indexOf('/'));
+//    } else {
+//      url = input;
+//    }
+//    return url;
+//  }
 
   static String getServerFilePath(String input) {
     String filePath;
@@ -79,11 +59,11 @@ public class Client {
   static void writeFile(DataInputStream in, DataOutputStream out) throws IOException {
     // Write the file out from the stream
     // -1 if end of stream
-    byte[] writeBytes = new byte[packetSize];
+    byte[] writeBytes = new byte[PACKET_SIZE];
     int response = in.read(writeBytes);
     while (response != -1) {
       out.write(writeBytes);
-      writeBytes = new byte[packetSize];
+      writeBytes = new byte[PACKET_SIZE];
       response = in.read(writeBytes);
     }
   }
@@ -91,6 +71,34 @@ public class Client {
   static void openFile(String filePath) throws IOException {
     Desktop.getDesktop().open(new File(filePath));
   }
+
+
+  private void downloadFile(String url) throws IOException {
+
+    System.out.println(url + optionalParameters);
+    try (Socket socket = new Socket(host, WEB_PORT)) {
+      // Instantiate input and output to send and receive from server.
+      // These do not need to be closed (closed does nothing according to documentation)
+      OutputStream socketOutputStream = socket.getOutputStream();
+      InputStream socketInputStream = socket.getInputStream();
+
+      try (DataInputStream dataInputStream = new DataInputStream(socketInputStream);
+          DataOutputStream dataOutputStream = new DataOutputStream(socketOutputStream)) {
+        System.out.println(url + optionalParameters);
+//        dataOutputStream.writeUTF("path: " + url + ", " + optionalParameters);
+        dataOutputStream.writeUTF("path:-" + url);
+
+        DataOutputStream fileOut = new DataOutputStream(
+            new FileOutputStream(new File(writePath)));
+        // Get the translated line back from the server.
+
+        writeFile(dataInputStream, fileOut);
+
+      }
+
+    }
+  }
+
 
   /**
    * Parses commandline arguments. First argument is a url, in the format
@@ -101,94 +109,108 @@ public class Client {
    *
    * @param args Arguments for running the program.
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
     if (args.length < 1 || args.length > 3) {
-      System.out.println("Must give minimum url argument in order to be a valid instance.");
+      System.out.println("Must give valid url argument and not too many arguments.");
     } else {
 
       try {
-        // Sending whole URL over now
-        String serverFilePath = getServerFilePath(args[0]);
-        String filePath = buildDefaultFilePath(args[0]);
-        System.out.println(filePath);
-        String parameters = "path: " + args[0];
+
+        Client client = new Client(args[0]);
         if (args.length == 1) {
-          addCacheFolder();
-        } else if (args.length == 2) {
-          if (args[1].toLowerCase().startsWith("headers=")) {
-
-          } else if (args[1].toLowerCase().startsWith("save=")) {
-
+          client.addDefaultCacheFolder();
+        }
+        if (args.length == 2) {
+          if (client.validCacheFolder(args[1])) {
+            client.setCacheFolder(args[1], "");
           } else {
-            System.out.println("Make sure that each argument corresponds to a valid key.");
+            client.setOptionalParameters(args[1], "");
           }
         } else if (args.length == 3) {
-          if (args[1].toLowerCase().startsWith("headers=") && args[2].toLowerCase()
-              .startsWith("save=")
-              || args[1].toLowerCase().startsWith("save=") && args[2].toLowerCase()
-              .startsWith("header=")) {
-            System.out.println(
-                "Make sure that each argument corresponds to a valid key and only one argument "
-                    + "should have each key.");
-          } else {
-            if (Files.isDirectory(Paths.get(args[1].toLowerCase().replace("save=", ""))) ||
-                Files.isDirectory(Paths.get(args[2].toLowerCase().replace("save=", "")))) {
+          client.setCacheFolder(args[1], args[2]);
+          client.setOptionalParameters(args[1], args[2]);
+        }
+        client.createDirectories();
 
-              // Should have special formatting for header/parameter string
-              parameters =
-                  args[1].contains("headers=") ? args[1].toLowerCase().replace("headers=", "")
-                      : args[2].toLowerCase().replace("headers=", "");
+        client.downloadFile(client.requestedFileString);
 
-              filePath = args[1].contains("save=") ? createDirectories(
-                  args[1].toLowerCase().replace("save=", ""), args[0])
-                  : createDirectories(args[2].toLowerCase().replace("save=", ""), args[0]);
-            } else {
-              System.out.println("Make sure valid directory path");
-            }
-          }
-
+        List<String> otherAssets =
+            new HTMLDependencyExtractor(client.writePath).getLinks(client.baseUrl);
+        System.out.println(otherAssets);
+        for (String link: otherAssets) {
+          // TODO Need to make valid write path as well
+          client.downloadFile(link);
         }
 
         // Arg 0 is the website url.
         // hello.testing.com/folder/file.html
 //        InetAddress host = InetAddress.getByName(getURL(args[0]));
 
-        // Localhost is 127.0.0.1
-        // TODO ****
-        InetAddress host = InetAddress.getByName("127.0.0.1");
+        // Open file in default program
+        // https://stackoverflow.com/questions/550329/how-to-open-a-file-with-the-default
+        //-associated-program
+        client.openFile(client.writePath);
+      } finally {
 
-        addCacheFolder();
-
-        try (Socket socket = new Socket(host, webPort)) {
-          // Instantiate input and output to send and receive from server.
-          // These do not need to be closed (closed does nothing according to documentation)
-          OutputStream socketOutputStream = socket.getOutputStream();
-          InputStream socketInputStream = socket.getInputStream();
-
-          try (DataInputStream dataInputStream = new DataInputStream(socketInputStream);
-              DataOutputStream dataOutputStream = new DataOutputStream(socketOutputStream)) {
-            dataOutputStream.writeUTF(parameters);
-
-            DataOutputStream fileOut = new DataOutputStream(
-                new FileOutputStream(new File(filePath)));
-            // Get the translated line back from the server.
-
-            writeFile(dataInputStream, fileOut);
-
-            // Open file in default program
-            // https://stackoverflow.com/questions/550329/how-to-open-a-file-with-the-default
-            //-associated-program
-            openFile(filePath);
-          }
-        }
-
-      } catch (IOException e) {
-        System.out.println(e);
-        // In future, let user know to run the server first
-        System.out.println("Unable to connect to client/server.");
       }
     }
+
+
+  }
+
+  private void createDirectories() throws IOException {
+    String[] folders = requestedFileString.split("/");
+    String pathSoFar = cacheFolder;
+    System.out.println(pathSoFar);
+    for (int i = 0; i < folders.length - 1; i++) {
+      pathSoFar = pathSoFar + File.separator + folders[i];
+
+      if (!Files
+          .isDirectory(Paths.get(pathSoFar))) {
+        Files.createDirectory(
+            Paths.get(pathSoFar));
+      }
+    }
+    pathSoFar = pathSoFar + File.separator + folders[folders.length - 1];
+    this.writePath = pathSoFar;
+  }
+
+  private void addDefaultCacheFolder() throws IOException {
+    if (!Files
+        .isDirectory(Paths.get(cacheFolder))) {
+      Files.createDirectory(
+          Paths.get(cacheFolder));
+    }
+  }
+
+  private void setCacheFolder(String arg1, String arg2) {
+    if (validCacheFolder(arg1) ^ validCacheFolder(arg2)) {
+      this.cacheFolder = validCacheFolder(arg1) ? arg1.toLowerCase().replace("save=", "") :
+          arg2.toLowerCase().replace("save=", "");
+    } else {
+      throw new IllegalArgumentException("Must provide one valid path");
+    }
+  }
+
+  private boolean validCacheFolder(String arg) {
+    return arg.toLowerCase().startsWith("save=") && Files
+        .isDirectory(Paths.get(arg.toLowerCase().replace("save=", "")));
+  }
+
+  private void setOptionalParameters(String arg1, String arg2) {
+    if (validHeader(arg1) ^ validHeader(arg2)) {
+      this.optionalParameters = validHeader(arg1) ? arg1.toLowerCase().replace("header=", "") :
+          arg2.toLowerCase().replace("header=", "");
+    } else {
+      throw new IllegalArgumentException("Must provide one valid path");
+    }
+
+  }
+
+  private boolean validHeader(String arg) {
+    return arg.toLowerCase().startsWith("header=");
+//    &&
   }
 
 }
--- Client/test/Client/ClientTest.java
+++ Client/test/Client/ClientTest.java
@@ -1,74 +1,74 @@
-package Client;
-
-import static Client.Client.buildDefaultFilePath;
-import static Client.Client.getServerFilePath;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.ref.Cleaner;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import org.junit.jupiter.api.Test;
-
-class ClientTest {
-  String sampleURL = "somecharacters.someothercharacters.com";
-  String sampleURLWithFile = "somecharacters.someothercharacters.com/folder/file.html";
-  String sampleRealURL = "www.geeksforgeeks.org/geeksforgeeks/GFG_Courses_Practice_GeeksforGeeks.html";
-
-  @Test
-  void test() throws UnknownHostException {
-    assertEquals(InetAddress.getByName("google.com").toString(), "google.com/172.217.165.142");
-  }
-
-  @Test
-  void testFormatFile() {
-    assertEquals(File.separator, "\\");
+//package Client;
+//
+//import static Client.Client.buildDefaultFilePath;
+//import static Client.Client.getServerFilePath;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.lang.ref.Cleaner;
+//import java.net.InetAddress;
+//import java.net.UnknownHostException;
+//import org.junit.jupiter.api.Test;
+//
+//class ClientTest {
+//  String sampleURL = "somecharacters.someothercharacters.com";
+//  String sampleURLWithFile = "somecharacters.someothercharacters.com/folder/file.html";
+//  String sampleRealURL = "www.geeksforgeeks.org/geeksforgeeks/GFG_Courses_Practice_GeeksforGeeks.html";
+//
+//  @Test
+//  void test() throws UnknownHostException {
+//    assertEquals(InetAddress.getByName("google.com").toString(), "google.com/172.217.165.142");
+//  }
+//
+//  @Test
+//  void testFormatFile() {
 //    assertEquals(File.separator, "\\");
-//    assertEquals(Client.buildFileName(), "c");
-  }
-
-  @Test
-  void testWriteFile() throws IOException {
-
-    DataOutputStream fileOut = new DataOutputStream(
-        new FileOutputStream(new File(buildDefaultFilePath(getServerFilePath(sampleURL)))));
-    DataInputStream fileIn = new DataInputStream(new FileInputStream(new File("src/Client/Client.java")));
-    Client.writeFile(fileIn, fileOut);
-    assertTrue(new File(buildDefaultFilePath(getServerFilePath(sampleURL))).isFile());
-    Client.openFile(buildDefaultFilePath(getServerFilePath(sampleURL)));
-  }
-
-  @Test
-  void testWriteWithPath() throws IOException {
-    DataOutputStream fileOut = new DataOutputStream(
-        new FileOutputStream(new File(buildDefaultFilePath(getServerFilePath(sampleURLWithFile)))));
-    DataInputStream fileIn = new DataInputStream(new FileInputStream(new File("src/Client/Client.java")));
-    Client.writeFile(fileIn, fileOut);
-    assertTrue(new File(buildDefaultFilePath(getServerFilePath(sampleURLWithFile))).isFile());
-    Client.openFile(buildDefaultFilePath(getServerFilePath(sampleURLWithFile)));
-  }
-
-  @Test
-  void testWriteReal() throws IOException {
-    DataOutputStream fileOut = new DataOutputStream(
-        new FileOutputStream(new File(buildDefaultFilePath(getServerFilePath(sampleRealURL)))));
-    DataInputStream fileIn = new DataInputStream(new FileInputStream(new File("src/Client/Client.java")));
-    Client.writeFile(fileIn, fileOut);
-    assertTrue(new File(buildDefaultFilePath(getServerFilePath(sampleRealURL))).isFile());
-    Client.openFile(buildDefaultFilePath(getServerFilePath(sampleRealURL)));
-  }
-
-
-  @Test
-  void testMain()
-  {
-    Client.main(new String[]{
-       sampleRealURL
-    });
-  }
-}
+////    assertEquals(File.separator, "\\");
+////    assertEquals(Client.buildFileName(), "c");
+//  }
+//
+//  @Test
+//  void testWriteFile() throws IOException {
+//
+//    DataOutputStream fileOut = new DataOutputStream(
+//        new FileOutputStream(new File(buildDefaultFilePath(getServerFilePath(sampleURL)))));
+//    DataInputStream fileIn = new DataInputStream(new FileInputStream(new File("src/Client/Client.java")));
+//    Client.writeFile(fileIn, fileOut);
+//    assertTrue(new File(buildDefaultFilePath(getServerFilePath(sampleURL))).isFile());
+//    Client.openFile(buildDefaultFilePath(getServerFilePath(sampleURL)));
+//  }
+//
+//  @Test
+//  void testWriteWithPath() throws IOException {
+//    DataOutputStream fileOut = new DataOutputStream(
+//        new FileOutputStream(new File(buildDefaultFilePath(getServerFilePath(sampleURLWithFile)))));
+//    DataInputStream fileIn = new DataInputStream(new FileInputStream(new File("src/Client/Client.java")));
+//    Client.writeFile(fileIn, fileOut);
+//    assertTrue(new File(buildDefaultFilePath(getServerFilePath(sampleURLWithFile))).isFile());
+//    Client.openFile(buildDefaultFilePath(getServerFilePath(sampleURLWithFile)));
+//  }
+//
+//  @Test
+//  void testWriteReal() throws IOException {
+//    DataOutputStream fileOut = new DataOutputStream(
+//        new FileOutputStream(new File(buildDefaultFilePath(getServerFilePath(sampleRealURL)))));
+//    DataInputStream fileIn = new DataInputStream(new FileInputStream(new File("src/Client/Client.java")));
+//    Client.writeFile(fileIn, fileOut);
+//    assertTrue(new File(buildDefaultFilePath(getServerFilePath(sampleRealURL))).isFile());
+//    Client.openFile(buildDefaultFilePath(getServerFilePath(sampleRealURL)));
+//  }
+//
+//
+//  @Test
+//  void testMain()
+//  {
+//    Client.main(new String[]{
+//       sampleRealURL
+//    });
+//  }
+//}
